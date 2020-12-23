f4b527613257ae1400b843d7b2dd30e31b8c963e
Gedalias Silva <sgedalias@gmail.com>
2020-12-23 17:29:53 -0300

    Construção do jogo.

---
+++ dio-snake/script.js
@@ -0,0 +1,54 @@
+let canvas = document.getElementByld("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right"; 
+
+function criarBG() {
+    context.fillStyle = "ligthgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        }
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keycode == 37 && direction != "right") direction = "left";
+    if(event.keycode == 38 && direction != "down") direction = "up";
+    if(event.keycode == 39 && direction != "left") direction = "right";
+    if(event.keycode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+    
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
