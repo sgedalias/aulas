a5b5578d3839424079bd6507204ae2164fd37eb7
Gedalias Silva <sgedalias@gmail.com>
2021-02-09 11:30:06 -0300

    Praticando exercícios com JavaScript.

---
+++ JavaScript-aprendizado/Aula11/ex008.js
@@ -0,0 +1,8 @@
+// Condição simples
+var vel = 12
+console.log(`A velocidade do carro é ${vel}Km/h.`)
+if (vel > 60) {
+    console.log(`Diminua a vlocidade. Você foi multado.`)
+}
+
+console.log(`Atenção! Dirija sempre usando o cinto de segurança!`)
